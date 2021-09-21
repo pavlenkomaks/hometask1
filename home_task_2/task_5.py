b847b86820567ba4c005cd005a06579edd134fa5
Mariya Yukhta <mariyayukhta@MacBook-Air-Mariya.local>
2021-09-21 21:04:59 +0300

    hg

--- home_task_2/task_5.py
+++ home_task_2/task_5.py
@@ -1,13 +1,13 @@
-# x = int(input("enter the number for counting the factorial: "))
-# f = 1
-# while n > 1:
-#    f = f * x
-#    x = n - 1
-# print(f)
-#
-
-import math
-
-n = int(input("enter the number: "))
-f = math.factorial(n)
+x = int(input("enter the number for counting the factorial: "))
+f = 1
+while x > 1:
+   f *= x
+   x -= 1
 print(f)
+
+#
+# import math
+#
+# n = int(input("enter the number: "))
+# f = math.factorial(n)
+# print(f)
